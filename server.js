b78d4088df4aa93fb12ce6fd297dc7b29c955698
tuansyho <emberfidestore@gmail.com>
2023-04-26 16:01:00 +0700

    video5

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from ERIC & HOI DAN IT </h2>');
-    res.end();
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+    res.send('hello tuan')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
