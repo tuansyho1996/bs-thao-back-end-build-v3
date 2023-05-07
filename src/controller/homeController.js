04107e9462227b5507536204dcec18d77bfb4ad9
tuansyho <emberfidestore@gmail.com>
2023-05-07 10:34:49 +0700

    video 13 complete CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -11,14 +11,32 @@ let getDetailPage = async (req, res) => {
     return res.send(JSON.stringify(user));
 }
 let createNewUser = async (req, res) => {
-    console.log('check req.body : ', req.body);
     let { firstName, lastName, email, address } = req.body;
-    console.log('check params:', firstName, lastName, email, address);
     await pool.execute('INSERT INTO user (firstName, lastName, email, address) values (?, ?, ?, ?)', [firstName, lastName, email, address]);
+    return res.redirect('/');
+}
+let deleteUser = async (req, res) => {
+    let userId = req.body.id;
+    await pool.execute('delete from user where id = ?', [userId]);
+    return res.redirect('/');
+}
+let getEditUser = async (req, res) => {
+    let userId = req.params.id;
+    const [user] = await pool.execute('SELECT * FROM user WHERE id = ?', [userId]);
+
+    res.render('editUser.ejs', { dataUser: user[0] });
+}
+let postEditUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+    console.log('check params:', firstName, lastName, email, address, id)
+    await pool.execute('update user set firstName = ?, lastName = ?, email = ?, address = ? where id = ?', [firstName, lastName, email, address, id]);
     return res.redirect('/');
 }
 module.exports = {
     getHomePage,
     getDetailPage,
-    createNewUser
+    createNewUser,
+    deleteUser,
+    getEditUser,
+    postEditUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -7,7 +7,10 @@ let router = express.Router();
 const initWebRouter = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/detail/user/:id', homeController.getDetailPage);
+    router.get('/edit-user/:id', homeController.getEditUser);
     router.post('/create-new-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
+    router.post('/edit-user', homeController.postEditUser);
     return app.use('/', router);
 }
 export default initWebRouter;
