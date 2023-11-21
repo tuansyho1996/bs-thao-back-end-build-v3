bec0a24cb7f983b99b75001caa11066ad0f6fe5f
tuansyho1996 <hosytuan101296@gmail.com>
2023-11-21 15:53:13 +0700

    update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,13 +1,42 @@
+// const { Sequelize } = require('sequelize');
+
+
+
+// // Option 3: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize('bs-thao', 'root', null, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     logging: false
+// });
+
+// let connectDB = async () => {
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Connection has been established successfully.');
+//     } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//     }
+// }
+// module.exports = connectDB;
+
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('bs-thao', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-});
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        logging: false,
+        timezone: "+07:00"
+    });
+
 
 let connectDB = async () => {
     try {
--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://14.225.254.19:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
